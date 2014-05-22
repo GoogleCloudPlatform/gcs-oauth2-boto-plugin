cd06b156dd32cfbfedcc06eb657c058050beeeb0
Bo Shi <bs@demeterr.com>
2014-05-21 21:27:14 -0700

    Rely on IP address for metadata server
    The client code uses the hard-coded IP and "metadata" to contact the
    GCE metadata host.  For maximum portability, don't rely on name
    resolution.

--- oauth2_plugin/oauth2_client.py
+++ oauth2_plugin/oauth2_client.py
@@ -65,14 +65,14 @@ token_exchange_lock = threading.Lock()
 
 DEFAULT_SCOPE = 'https://www.googleapis.com/auth/devstorage.full_control'
 
-META_TOKEN_URI = ('http://metadata/computeMetadata/v1/instance/'
-                  'service-accounts/default/token')
-
 META_HEADERS = {
     'X-Google-Metadata-Request': 'True'
 }
 
 METADATA_SERVER = 'http://169.254.169.254'
+
+META_TOKEN_URI = METADATA_SERVER + ('/computeMetadata/v1/instance/'
+                                    'service-accounts/default/token')
 
 
 # Note: this is copied from gsutil's gslib.cred_types. It should be kept in
