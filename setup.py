335d08df97a9af691bb6ad70c972d077ca1718fb
Travis Hobrla <thobrla@google.com>
2015-04-14 13:29:45 -0700

    Update version to 1.9.

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ requires = [
 
 setup(
     name='gcs-oauth2-boto-plugin',
-    version='1.8',
+    version='1.9',
     url='https://developers.google.com/storage/docs/gspythonlibrary',
     download_url=('https://github.com/GoogleCloudPlatform'
                   '/gcs-oauth2-boto-plugin'),
