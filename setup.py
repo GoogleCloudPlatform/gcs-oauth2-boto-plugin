aa52aa86210af6a2107cb00e65c55baba3ca82a4
Travis Hobrla <thobrla@google.com>
2016-02-22 10:14:53 -0800

    Pin to oauth2client 1.5.2, bump version to 1.10
    This change also drops the google-api-python-client dependency,
    which was only being used for oauth2client.
    
    Fixes https://github.com/GoogleCloudPlatform/gsutil/issues/333

--- setup.py
+++ setup.py
@@ -25,15 +25,14 @@ gcs-oauth2-boto-plugin is a Python application whose purpose is to behave as an
 auth plugin for the boto auth plugin framework for use with OAuth 2.0
 credentials for the Google Cloud Platform. This plugin is compatible with both
 user accounts and service accounts, and its functionality is essentially a
-wrapper around the oauth2client package of google-api-python-client with the
-addition of automatically caching tokens for the machine in a thread- and
-process-safe fashion.
+wrapper around oauth2client with the addition of automatically caching tokens
+for the machine in a thread- and process-safe fashion.
 """
 
 requires = [
     'boto>=2.29.1',
     'httplib2>=0.8',
-    'google-api-python-client>=1.1',
+    'oauth2client==1.5.2',
     'pyOpenSSL>=0.13',
     # Not using 1.02 because of:
     #   https://code.google.com/p/socksipy-branch/issues/detail?id=3
@@ -43,7 +42,7 @@ requires = [
 
 setup(
     name='gcs-oauth2-boto-plugin',
-    version='1.9',
+    version='1.10',
     url='https://developers.google.com/storage/docs/gspythonlibrary',
     download_url=('https://github.com/GoogleCloudPlatform'
                   '/gcs-oauth2-boto-plugin'),
