8d517db01a6bb7f38435919adb53384e869de532
Roman Gershman <romange@gmail.com>
2016-05-04 14:04:50 +0300

    Fix support for oauth2client 2.0

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ for the machine in a thread- and process-safe fashion.
 requires = [
     'boto>=2.29.1',
     'httplib2>=0.8',
-    'oauth2client==1.5.2',
+    'oauth2client>=1.5.2',
     'pyOpenSSL>=0.13',
     # Not using 1.02 because of:
     #   https://code.google.com/p/socksipy-branch/issues/detail?id=3
