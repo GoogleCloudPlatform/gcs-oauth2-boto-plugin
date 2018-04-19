0221e03dadfeff74072e310d7d924bb2fcd3a7f6
Matt Houglum <houglum@google.com>
2018-04-19 12:01:36 -0700

    Update version to 2.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ extras_require = {
 
 setup(
     name='gcs-oauth2-boto-plugin',
-    version='1.14',
+    version='2.0',
     url='https://developers.google.com/storage/docs/gspythonlibrary',
     download_url=('https://github.com/GoogleCloudPlatform'
                   '/gcs-oauth2-boto-plugin'),
